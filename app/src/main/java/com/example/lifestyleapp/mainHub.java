43738520ed36a11069ca3512f31bedf471876d0a
EDuffy1998 <60000514+EDuffy1998@users.noreply.github.com>
2020-05-01 14:05:17 +0100

    Added functionality to main hub / classes need to be added to intents Added feature where user can upload a profile picture

--- app/src/main/java/com/example/lifestyleapp/mainHub.java
+++ app/src/main/java/com/example/lifestyleapp/mainHub.java
@@ -1,42 +1,136 @@
 package com.example.lifestyleapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class mainHub extends AppCompatActivity {
 
-    // Firebase variable
     private FirebaseAuth mAuth;
-    //Buttons
-    private Button logout;
+    private StorageReference mStorageRef;
+    private TextView email;
+    private TextView fullName;
+    private ImageView pic;
+    private String url;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hub);
         mAuth = FirebaseAuth.getInstance();
-    }
-    public void logout (android.view.View view) {
+        final FirebaseUser user = mAuth.getCurrentUser();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        fullName = findViewById(R.id.fullName);
+        email = findViewById(R.id.email);
+        pic = findViewById(R.id.imageView);
 
-        try {
-            // sign out
-            mAuth.signOut();
-            // retutn to login
-            Intent myIntent = new Intent(getBaseContext(), Login.class);
-            startActivity(myIntent);
-            finish();
+        if (user != null) {
+            String email1 = user.getEmail();
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("users/" + user.getUid());
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    DataSnapshot snapshot = dataSnapshot.child("Name");
+                    String name = snapshot.getValue().toString();
+                    fullname.setText(name);
+                }
 
-        } catch (Exception e) {
-            // an error
-            //TODO handle this error
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+            email.setText(email1);
+            try {
+                url = user.getUid();
+                final File localFile = File.createTempFile(url, "jpg");
+                StorageReference Ref = mStorageRef.child(url + ".jpg");
+                Ref.getFile(localFile)
+                        .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                // Successfully downloaded data to local file
+                                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                                pic.setImageBitmap(bitmap);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle failed download
+                        // ...
+                    }
+                });
+            } catch (Exception e) {
+            }
+
         }
+    }
 
 
+
+    public void cart(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),"class name".class);
+
+    }
+
+    public void message(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),.class);
+        startActivity(myIntent);
+    }
+
+    public void groupMessage(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),.class);
+        startActivity(myIntent);
+    }
+
+    public void priceCheck(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),.class);
+        startActivity(myIntent);
+    }
+
+    public void closestShop(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),.class);
+        startActivity(myIntent);
+    }
+
+    public void statistics(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),.class);
+        startActivity(myIntent);
+    }
+
+    public void logout(android.view.View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(myIntent);
+
+    }
+
+    public void share(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(),"share".class);
+        startActivity(myIntent);
     }
 }
+
---
+++ app/src/main/java/com/example/lifestyleapp/uploadPhoto.java
@@ -0,0 +1,100 @@
+package com.example.lifestyleapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class uploadPhoto extends AppCompatActivity {
+    ImageView img;
+    private StorageReference mStorageRef;
+    private FirebaseAuth mAuth;
+    public Uri imguri;
+    private String url;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        img = (ImageView) findViewById(R.id.profile);
+    }
+
+    public void chooseFile(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imguri = data.getData();
+            img.setImageURI(imguri);
+        }
+    }
+
+    public void uploadFile(android.view.View view){
+        //Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        //StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+        url = user.getUid();
+        System.out.println(url);
+        StorageReference Ref = mStorageRef.child(url+"."+getExtension(imguri));
+
+        Ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+        updateUI();
+    }
+
+    private String getExtension(Uri uri) {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    public void updateUI(){
+        Intent myIntent = new Intent(getBaseContext(), mainHub.class);
+        startActivity(myIntent);
+
+    }
+
+
+
+
+
+
+}
+
