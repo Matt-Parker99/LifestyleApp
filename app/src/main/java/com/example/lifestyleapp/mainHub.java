408b65229faae119bb1a6c5ca46b9bda0a4ea37a
markog1999 <55714310+markog1999@users.noreply.github.com>
2020-05-01 01:20:54 +0100

    Added logout function to "back" button in the main hub

--- app/src/main/java/com/example/lifestyleapp/mainHub.java
+++ app/src/main/java/com/example/lifestyleapp/mainHub.java
@@ -1,14 +1,42 @@
 package com.example.lifestyleapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class mainHub extends AppCompatActivity {
+
+    // Firebase variable
+    private FirebaseAuth mAuth;
+    //Buttons
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hub);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    public void logout (android.view.View view) {
+
+        try {
+            // sign out
+            mAuth.signOut();
+            // retutn to login
+            Intent myIntent = new Intent(getBaseContext(), Login.class);
+            startActivity(myIntent);
+            finish();
+
+        } catch (Exception e) {
+            // an error
+            //TODO handle this error
+        }
+
+
     }
 }
