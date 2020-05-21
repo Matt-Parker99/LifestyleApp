d51ca85ca73906dee984ce5befb1af6f6e81df84
markog1999 <55714310+markog1999@users.noreply.github.com>
2020-05-21 23:34:01 +0100

    PriceCheck activity ow loads ingredient objects from database and extracts {name, price} into listview

--- app/src/main/java/com/example/lifestyleapp/PriceCheck.java
+++ app/src/main/java/com/example/lifestyleapp/PriceCheck.java
@@ -1,24 +1,122 @@
 package com.example.lifestyleapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class PriceCheck extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+    private FirebaseUser user;
+    private FirebaseFirestore db;
+
+
     ListView listView;
-    TextView textView;
-    String[] listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price_check);
 
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        listView = findViewById(R.id.productsView);
+
+        final Context myContext = this;
+
+
+        db.collection("products")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int listSize = task.getResult().size();
+                            String title[] = new String[listSize];
+                            String price[] = new String[listSize];
+
+                            int i = 0;
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                title[i] = document.get("name").toString();
+                                price[i] = document.get("price").toString();
+                                i += 1;
+                            }
+                            MyAdapter adapter = new MyAdapter(myContext, title, price);
+                            listView.setAdapter(adapter);
+                        } else {
+                            Log.e("PriceCheck", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+        // now set item click on list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        // so item click is done now check list view
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String title[];
+        String price[];
+
+
+        MyAdapter (Context c, String title[], String price[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.title = title;
+            this.price = price;
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            // now set our resources on views
+            myTitle.setText(title[position]);
+            myDescription.setText(price[position]);
 
 
 
+
+            return row;
+        }
     }
 }
--- app/src/main/java/com/example/lifestyleapp/avocadoOnToast.java
+++ app/src/main/java/com/example/lifestyleapp/avocadoOnToast.java
@@ -1,18 +1,15 @@
 package com.example.lifestyleapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.annotations.Nullable;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -30,7 +27,7 @@ public class avocadoOnToast extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_avocado_on_toast);
+        //setContentView(R.layout.activity_avocado_on_toast);
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         mStorageRef = FirebaseStorage.getInstance().getReference();
