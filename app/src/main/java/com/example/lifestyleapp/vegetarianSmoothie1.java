49643adaab5a58fd36cefe21751e0ef73cd2c070
EDuffy1998 <60000514+EDuffy1998@users.noreply.github.com>
2020-07-14 14:10:34 +0100

    Completed the smoothie section for the vegetarian section, all mapping is correct between activities and all that needs to be done is the layout and images but all code and functionality is complete
    - Edward

---
+++ app/src/main/java/com/example/lifestyleapp/vegetarianSmoothie1.java
@@ -0,0 +1,79 @@
+package com.example.lifestyleapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
+public class vegetarianSmoothie1 extends AppCompatActivity {
+
+    private String userID;
+    private FirebaseUser user;
+    private StorageReference mStorageRef;
+    private FirebaseAuth mAuth;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String ingredients[] = {"100ml full-fat milk ","2 tbsp natural yogurt","1 banana","150g frozen fruits of the forest","50g blueberries","1 tbsp chia seeds","½ tsp cinnamon","1 tbsp goji berries","1 tsp mixed seeds","1 tsp honey"};
+    // quantity units to bo added in the ingredient object file on DB
+    Double quantaties[] = {100.0, 2.0 , 1.0, .150, 0.50, 1.0, 0.50,1.0,1.0,1.0};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vegetarian_smoothie1);
+
+    }
+
+//    public void back(android.view.View view){
+//        Intent myIntent = new Intent(getBaseContext(), vegetarianSmoothies.class);
+//        startActivity(myIntent);
+//    };
+
+    public void viewRecipeOnline(android.view.View view){
+        Toast.makeText(vegetarianSmoothie1.this, "Accessing Irish Independent Website", Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bbcgoodfood.com/recipes/breakfast-super-shake"));
+        startActivity(myIntent);
+    }
+
+
+    public void addToShoppingList(android.view.View view){
+        // Identify the user
+        // Iterate through the list of ingredients
+        // Perform insert statements adding the ingredients to a row of the users table which we can display in another activity
+        userID = user.getUid();
+        //final DocumentReference userDocRef = db.collection("users").document(userID);
+        try {
+            int i = 0;
+            for(String ingredient : ingredients) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference newIngedientRef = database.getReference("users/" + userID + "/list").push();
+                newIngedientRef.setValue("id", ingredient);
+                newIngedientRef.setValue("quantity", quantaties[i]);
+                i += 1;
+            }
+            // Success message
+            Toast.makeText(getApplicationContext(), "Successfully Added To Shopping List!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        };
+    };
+
+    public void home(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(), MainHub.class);
+        startActivity(myIntent);
+    };
+
+
+    public void share(android.view.View view){
+        // Havent developed functionality for this yet.
+    };
+}
