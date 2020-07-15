db4837c2b2b2f388f357cab08800fe6713a9475a
EDuffy1998 <60000514+EDuffy1998@users.noreply.github.com>
2020-07-15 11:09:46 +0100

    Completed the breakfast section for the vegetarian section, all mapping is correct between activities and all that needs to be done is the layout design but all code and functionality is complete
    - Edward

---
+++ app/src/main/java/com/example/lifestyleapp/vegBreakfastRecipe4.java
@@ -0,0 +1,86 @@
+package com.example.lifestyleapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
+public class vegBreakfastRecipe4 extends AppCompatActivity {
+
+    private String userID;
+    private FirebaseUser user;
+    private StorageReference mStorageRef;
+    private FirebaseAuth mAuth;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String ingredients[] = {"2 slices of white bread"
+            ,"knob of butter"
+            ,"25g cheddar"
+            ,"small handful baby spinach"
+            ,"1 tbsp olive oil"
+            ,"1 medium egg"
+            ,"sriracha hot sauce"
+            };
+    // quantity units to bo added in the ingredient object file on DB
+    Double quantaties[] = {2.0,1.0,0.025,1.0,1.0,1.0,1.0};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veg_breakfast_recipe4);
+
+    }
+
+//    public void back(android.view.View view){
+//        Intent myIntent = new Intent(getBaseContext(), vegetarianSmoothies.class);
+//        startActivity(myIntent);
+//    };
+
+    public void viewRecipeOnline(android.view.View view){
+        Toast.makeText(vegBreakfastRecipe4.this, "Accessing Recipe Website", Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bbcgoodfood.com/recipes/fried-egg-florentine-toastie"));
+        startActivity(myIntent);
+    }
+
+
+    public void addToShoppingList(android.view.View view){
+        // Identify the user
+        // Iterate through the list of ingredients
+        // Perform insert statements adding the ingredients to a row of the users table which we can display in another activity
+        userID = user.getUid();
+        //final DocumentReference userDocRef = db.collection("users").document(userID);
+        try {
+            int i = 0;
+            for(String ingredient : ingredients) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference newIngedientRef = database.getReference("users/" + userID + "/list").push();
+                newIngedientRef.setValue("id", ingredient);
+                newIngedientRef.setValue("quantity", quantaties[i]);
+                i += 1;
+            }
+            // Success message
+            Toast.makeText(getApplicationContext(), "Successfully Added To Shopping List!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        };
+    };
+
+    public void home(android.view.View view){
+        Intent myIntent = new Intent(getBaseContext(), MainHub.class);
+        startActivity(myIntent);
+    };
+
+
+    public void share(android.view.View view){
+        // Havent developed functionality for this yet.
+    };
+}
